11433a4404bfbb1bb434ba3a3931ec3814101299
abmulla2005 <abmulla2005@gmail.com>
2026-06-28 09:28:50 +0530

    first commit

---
+++ langgraph_database_backend.py
@@ -0,0 +1,55 @@
+from langgraph.graph import StateGraph,START,END
+from typing import TypedDict,Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import sqlite3
+import os
+
+
+load_dotenv()
+
+api_key = os.getenv("OPEN_ROUTER_API_KEY3")
+
+# print(api_key)
+
+# llm = Chatnousresearch/hermes-3-llama-3.1-405b:freeOpenAI(
+#     model="meta-llama/llama-3.3-70b-instruct:free",
+#     api_key=api_key
+# )
+
+llm = ChatOpenAI(
+   model="google/gemma-4-31b-it:free",
+   base_url="https://openrouter.ai/api/v1",
+   api_key=api_key
+)
+
+print(llm.invoke("Hello"))
+
+class ChatState(TypedDict):
+    messages:Annotated[list[BaseMessage],add_messages]
+
+def chat_node(state:ChatState):
+    messages =state['messages']
+    response = llm.invoke(messages)
+    return {"message":[response]}
+
+conn = sqlite3.connect(database='chatbot.db',check_same_thread=False)
+
+checkpointer = SqliteSaver(conn=conn)
+
+graph = StateGraph(ChatState)
+
+graph.add_node("chat_node",chat_node)
+graph.add_edge(START,"chat_node")
+graph.add_edge("chat_node",END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+    return list(all_threads)
