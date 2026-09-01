cb751e459d6879e5b40038fdcfd476939c89f362
Abrar Mulla <155132977+abmulla2005@users.noreply.github.com>
2026-09-01 08:34:59 +0530

    Update langgraph_database_backend.py

--- langgraph_database_backend.py
+++ langgraph_database_backend.py
@@ -32,7 +32,7 @@ if not api_key:
 # LLM
 # ----------------------------------------------------
 llm = ChatOpenAI(
-    model="openai/gpt-oss-20b:free",
+    model="google/gemma-4-31b-it:free",
     base_url="https://openrouter.ai/api/v1",
     api_key=api_key,
     temperature=0.7,
